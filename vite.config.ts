ea72c5ff4628b8059b018077c3e8bca975b62aee
Romeo-mz <romeo.martinez@orange.fr>
2025-09-22 19:13:19 +0200

    add cname

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ndm-website/', // GitHub Pages base path
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
